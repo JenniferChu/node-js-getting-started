f0dfec5cc6e068d39bb0ef63bd6070dfe2c2987c
Jennifer Chu <jchu1996@gmail.com>
2016-08-03 20:10:10 -0400

    trying to just make a fake pathname, then load all files in public

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.use(express.static(__dirname + '/public'));
 app.use('/lab8', express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -18,11 +18,11 @@ app.get('/', function(request, response) {
 app.get('/cool', function(request, response) {
   response.send(cool());
 });
-
+/*
 app.get('/lab8', function(request, response) {
   response.sendFile(__dirname/public/lab8.html);
 });
-
+*/
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });

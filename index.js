dc9a626270b1a8feff4b851dccce39e6cf6da7d6
Jennifer Chu <jchu1996@gmail.com>
2016-08-04 11:26:39 -0400

    trying an app.use(express.static()) inside of the app.get

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/lab8', express.static('public'));
-app.use(express.static(__dirname + '/public'));
+//app.use('/lab8', express.static('public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -18,11 +17,12 @@ app.get('/', function(request, response) {
 app.get('/cool', function(request, response) {
   response.send(cool());
 });
-/*
-app.get('/lab8', function(request, response) {
-  response.sendFile(__dirname/public/lab8.html);
+
+app.get('/lab8', function() {
+  //response.sendFile(__dirname/public/lab8.html);
+  app.use(express.static(__dirname + '/public'));
 });
-*/
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });

93bd903b696ea82fe14ee7897ba6f44268c380ab
Jennifer Chu <jchu1996@gmail.com>
2016-08-05 17:14:55 -0400

    adding mbta server functionality (non-working)

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
+var cors = require('cors');
+app.use(cors());
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mbta_db';
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
+	db = databaseConnection;
+});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use('/lab8', express.static(__dirname + '/public'));
@@ -24,6 +33,22 @@ app.get('/lab8', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/redline.json', function(request, response) {
+	var req = new XMLHttpRequest();
+	req.open("GET", "http://developer.mbta.com/lib/rthr/red.json", true);
+	req.onreadystatechange = get_schedule;
+	req.send(null);
+
+	var jsondata;
+	function get_schedule() {
+		if (req.readyState == 4 && req.status == 200) {
+			jsondata = req.responseText;
+		}
+	}
+	response.set('Content-Type', 'text/plain');
+	response.send(jsondata);
 });
 
 
